1ab2b19edf2a9689bb658374809577c82429cc2c
GarikMta <gariktheteacher@gmail.com>
2022-09-17 23:45:53 +0400

    StartJavaStudies

---
+++ src/Package1/TestClass.java
@@ -0,0 +1,10 @@
+package Package1;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.print("Knock-knock ");
+        System.out.println("The Matrix has you");
+        System.out.println("Time to wake up");
+
+    }
+}
